0820208b464742a6884fcf39513652c638d5822b
Sisun Cheng <76165424+sscheng25@users.noreply.github.com>
2021-12-04 23:02:16 -0500

    Update transform_data.py

--- html/src/transform_data.py
+++ html/src/transform_data.py
@@ -2,7 +2,7 @@
 # Setting environment variable. NOTE: This is not a production-safe practice.
 # This is only acceptable because this is a lab.
 import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/tushimin/Desktop/musa-509-final-27b36afe08d2.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/CSS/Desktop/musa-509-final-27b36afe08d2.json'
 #####\
 
 from pathlib import Path
@@ -15,6 +15,10 @@ def main():
     run_transform_gbq('justtest', 'cri_in_neigh', sql_root)
     run_transform_gbq('justtest', 'gro_in_neigh', sql_root)
     run_transform_gbq('justtest', 'res_in_neigh', sql_root)
+    run_transform_gbq('justtest', 'neighborhood_list', sql_root)
+    run_transform_gbq('justtest', 'nhoods_detail', sql_root)
+    run_transform_gbq('justtest', 'max_crime', sql_root)
+    run_transform_gbq('justtest', 'min_crime', sql_root)
 
 if __name__ == '__main__':
     main()
